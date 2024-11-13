dcee570be0430a70140afefb4a41560513c7f904
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2024-11-13 06:25:15 +0100

    add range

--- src/Routes/Base.php
+++ src/Routes/Base.php
@@ -58,5 +58,52 @@ class Base implements IRoute{
                 TualoApplication::result('msg', $e->getMessage());
             }
         },array('get','post'),true);
+
+        Route::add('/currencyapi/historical/range/(?P<year>[\d\-\_]+)/(?P<currencies>[\w\-\_]+)',function($matches){
+            TualoApplication::contenttype('application/json');
+            try{
+
+
+
+                $x = [];
+                $date = new \DateTimeImmutable($matches['year'].'-01-01');
+                $interval = new \DateInterval('P1D');
+
+                for ($i=0;$i<366;$i++){
+                    
+
+                    // $x[] = $date->format('Y-m-d');
+                    $date = $date->add($interval);
+                    
+                    $result = API::getDate(
+                        strtotime($date->format('Y-m-d')),
+                        'EUR',
+                        explode('-',$matches['currencies'])
+                    );
+
+                    $sql ='insert into currencyapi_historical_day 
+                    (base_currency,target_currency,`date`,rate) 
+                    values ({base_currency},{target_currency},{date},{rate})
+                    on duplicate key update rate=values(rate)';
+                    foreach($result['data'] as $key=>$r){
+                        TualoApplication::get('session')->getDB()->direct($sql,[
+                            'base_currency'=>'EUR',
+                            'target_currency'=>$r['code'],
+                            'date'=>$date->format('Y-m-d'),
+                            'rate'=>$r['value']
+                        ]);
+
+                    }
+                    $x[]=$result;
+                    
+                }
+                TualoApplication::result('time',time() );
+                TualoApplication::result('data',$x );
+                TualoApplication::result('success',true );
+
+            }catch(\Exception $e){
+                TualoApplication::result('msg', $e->getMessage());
+            }
+        },array('get','post'),true);
     }
 }
