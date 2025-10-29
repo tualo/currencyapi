e3933bd7b10924d80d03df428607a7e4a28a26b7
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:38:50 +0100

    Version bump to 1.0.2

--- src/Routes/Base.php
+++ src/Routes/Base.php
@@ -1,109 +1,108 @@
 <?php
+
 namespace Tualo\Office\CurrencyAPI\Routes;
+
 use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\Basic\Route;
 use Tualo\Office\Basic\IRoute;
 use Tualo\Office\CurrencyAPI\API;
 
 
-class Base implements IRoute{
-    public static function register(){
+class Base extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
 
-        Route::add('/currencyapi/historical/(?P<start>[\d\-\_]+)/(?P<stop>[\d\-\_]+)/(?P<currencies>[\w\-\_]+)',function($matches){
+        Route::add('/currencyapi/historical/(?P<start>[\d\-\_]+)/(?P<stop>[\d\-\_]+)/(?P<currencies>[\w\-\_]+)', function ($matches) {
             TualoApplication::contenttype('application/json');
-            try{
+            try {
                 $result = API::getDateRange(
                     strtotime($matches['start']),
                     strtotime($matches['stop']),
                     'EUR',
-                    explode('-',$matches['currencies'])
+                    explode('-', $matches['currencies'])
                 );
 
-                TualoApplication::result('time',time() );
-                TualoApplication::result('data',$result );
-                TualoApplication::result('success',true );
-
-            }catch(\Exception $e){
+                TualoApplication::result('time', time());
+                TualoApplication::result('data', $result);
+                TualoApplication::result('success', true);
+            } catch (\Exception $e) {
                 TualoApplication::result('msg', $e->getMessage());
             }
-        },array('get','post'),true);
+        }, array('get', 'post'), true);
 
 
-        Route::add('/currencyapi/historical/(?P<date>[\d\-\_]+)/(?P<currencies>[\w\-\_]+)',function($matches){
+        Route::add('/currencyapi/historical/(?P<date>[\d\-\_]+)/(?P<currencies>[\w\-\_]+)', function ($matches) {
             TualoApplication::contenttype('application/json');
-            try{
+            try {
                 $result = API::getDate(
                     strtotime($matches['date']),
                     'EUR',
-                    explode('-',$matches['currencies'])
+                    explode('-', $matches['currencies'])
                 );
 
-                $sql ='insert into currencyapi_historical_day 
+                $sql = 'insert into currencyapi_historical_day 
                 (base_currency,target_currency,`date`,rate) 
                 values ({base_currency},{target_currency},{date},{rate})
                 on duplicate key update rate=values(rate)';
-                foreach($result['data'] as $key=>$r){
-                    TualoApplication::get('session')->getDB()->direct($sql,[
-                        'base_currency'=>'EUR',
-                        'target_currency'=>$r['code'],
-                        'date'=>$matches['date'],
-                        'rate'=>$r['value']
+                foreach ($result['data'] as $key => $r) {
+                    TualoApplication::get('session')->getDB()->direct($sql, [
+                        'base_currency' => 'EUR',
+                        'target_currency' => $r['code'],
+                        'date' => $matches['date'],
+                        'rate' => $r['value']
                     ]);
                 }
-                TualoApplication::result('time',time() );
-                TualoApplication::result('data',$result );
-                TualoApplication::result('success',true );
-
-            }catch(\Exception $e){
+                TualoApplication::result('time', time());
+                TualoApplication::result('data', $result);
+                TualoApplication::result('success', true);
+            } catch (\Exception $e) {
                 TualoApplication::result('msg', $e->getMessage());
             }
-        },array('get','post'),true);
+        }, array('get', 'post'), true);
 
-        Route::add('/currencyapi/historical/range/(?P<year>[\d\-\_]+)/(?P<currencies>[\w\-\_]+)',function($matches){
+        Route::add('/currencyapi/historical/range/(?P<year>[\d\-\_]+)/(?P<currencies>[\w\-\_]+)', function ($matches) {
             TualoApplication::contenttype('application/json');
-            try{
+            try {
 
 
 
                 $x = [];
-                $date = new \DateTimeImmutable($matches['year'].'-01-01');
+                $date = new \DateTimeImmutable($matches['year'] . '-01-01');
                 $interval = new \DateInterval('P1D');
 
-                for ($i=0;$i<366;$i++){
-                    
+                for ($i = 0; $i < 366; $i++) {
+
 
                     // $x[] = $date->format('Y-m-d');
                     $date = $date->add($interval);
-                    
+
                     $result = API::getDate(
                         strtotime($date->format('Y-m-d')),
                         'EUR',
-                        explode('-',$matches['currencies'])
+                        explode('-', $matches['currencies'])
                     );
 
-                    $sql ='insert into currencyapi_historical_day 
+                    $sql = 'insert into currencyapi_historical_day 
                     (base_currency,target_currency,`date`,rate) 
                     values ({base_currency},{target_currency},{date},{rate})
                     on duplicate key update rate=values(rate)';
-                    foreach($result['data'] as $key=>$r){
-                        TualoApplication::get('session')->getDB()->direct($sql,[
-                            'base_currency'=>'EUR',
-                            'target_currency'=>$r['code'],
-                            'date'=>$date->format('Y-m-d'),
-                            'rate'=>$r['value']
+                    foreach ($result['data'] as $key => $r) {
+                        TualoApplication::get('session')->getDB()->direct($sql, [
+                            'base_currency' => 'EUR',
+                            'target_currency' => $r['code'],
+                            'date' => $date->format('Y-m-d'),
+                            'rate' => $r['value']
                         ]);
-
                     }
-                    $x[]=$result;
-                    
+                    $x[] = $result;
                 }
-                TualoApplication::result('time',time() );
-                TualoApplication::result('data',$x );
-                TualoApplication::result('success',true );
-
-            }catch(\Exception $e){
+                TualoApplication::result('time', time());
+                TualoApplication::result('data', $x);
+                TualoApplication::result('success', true);
+            } catch (\Exception $e) {
                 TualoApplication::result('msg', $e->getMessage());
             }
-        },array('get','post'),true);
+        }, array('get', 'post'), true);
     }
 }
